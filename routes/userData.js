4b28c359bc3fdf8e2b359a02a0a45786551a4d3b
Sergey-air-749 <michinssergeevich@gmail.com>
2026-01-17 20:09:01 +0500

    17.01.26 20:09 fix avatar 2

--- routes/userData.js
+++ routes/userData.js
@@ -89,18 +89,18 @@ router.get('/getUserDataById/:id', async (req, res, next) => {
 });
 
 
-// router.get('/images/avatars/:id', async (req, res, next) => {
-//     const { id } = req.params
+router.get('/images/avatars/:id', async (req, res, next) => {
+    const { id } = req.params
 
-//     try {
+    try {
 
-//         const usersAvatars = path.join(__dirname, '../avatars', `${id}.png`)
-//         console.log(usersAvatars);
-//         res.sendFile(usersAvatars)
-//     } catch (error) {
-//         res.status(500).json({msg: error.message})
-//     }
-// });
+        const usersAvatars = path.join(__dirname, '../avatars', `${id}.png`)
+        console.log(usersAvatars);
+        res.sendFile(usersAvatars)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+});
 
 
 router.post('/account/delete', authMidelwares, async (req, res, next) => {
