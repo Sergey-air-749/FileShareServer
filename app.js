4870a08ff7a14b47bad24cc5d193dec22d057f3d
Sergey-air-749 <michinssergeevich@gmail.com>
2026-01-19 22:11:42 +0500

    19.01.26 22:11 mongoose fix 4

--- app.js
+++ app.js
@@ -62,67 +62,75 @@ const userFileStory = require('./routes/userFileStory');
 
 
 
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {},
-    cors: {
-        // Разрешаем подключения с клиентского домена/порта
-        origin: "https://fileshare-one-rust.vercel.app", // <-- Не в коем случи не ставить в конце "/" !!!!!!
-        methods: ["GET", "POST"],
-    }
-});
-
-
-io.on('connection', async (socket) => {
-    console.log('a user connected');
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
-    socket.on('pingfilesShareId', async (shareId) => {
-        socket.join(shareId);
-         
-
-        const user = await users.findOne({shareId: shareId}) 
-        const files = user.filse
-        
-        io.to(shareId).emit("files", files);
-    });
-
-    socket.on('pingfilesUserName', async (username) => {
-         
-        const user = await users.findOne({username: username}) 
-
-        socket.join(user.shareId);
-
-        const files = user.filse
-        
-        io.to(user.shareId).emit("files", files);
-    });
-
-});
-
 async function startMongoDBConnected() {
+
     await connectDB()
     console.log('MongoDB connected')
-}
+    
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        cors: {
+            // Разрешаем подключения с клиентского домена/порта
+            origin: "https://fileshare-one-rust.vercel.app", // <-- Не в коем случи не ставить в конце "/" !!!!!!
+            methods: ["GET", "POST"],
+        }
+    });
+    
+    
+    io.on('connection', async (socket) => {
+        console.log('a user connected');
+    
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+    
+        socket.on('pingfilesShareId', async (shareId) => {
+            socket.join(shareId);
+             
+    
+            const user = await users.findOne({shareId: shareId}) 
+            const files = user.filse
+            
+            io.to(shareId).emit("files", files);
+        });
+    
+        socket.on('pingfilesUserName', async (username) => {
+             
+            const user = await users.findOne({username: username}) 
+    
+            socket.join(user.shareId);
+    
+            const files = user.filse
+            
+            io.to(user.shareId).emit("files", files);
+        });
+    
+    });
+
+
+    
+    app.use(cors())
+    
+    app.use(express.json());
+    app.use(express.json({ limit: '1000mb' }));
+    app.use(express.urlencoded({ limit: '1000mb', extended: true }));
+    
+    app.use('/api', emailRoutes, changeUserData, filesRoutes, userData, userFileStory, authRoutes)
+    
+    server.listen(process.env.PORT_API, () => {
+        console.log("API and Soket IO http://localhost:" + process.env.PORT_API);
+    });
+    
+    
+
+    console.log(os.hostname());
+    console.log(os.networkInterfaces());
+    console.log(os.userInfo());
+    console.log(os.version());
+}    
 
 startMongoDBConnected()
-
-app.use(cors())
-
-app.use(express.json());
-app.use(express.json({ limit: '1000mb' }));
-app.use(express.urlencoded({ limit: '1000mb', extended: true }));
-
-app.use('/api', emailRoutes, changeUserData, filesRoutes, userData, userFileStory, authRoutes)
-
-server.listen(process.env.PORT_API, () => {
-    console.log("API and Soket IO http://localhost:" + process.env.PORT_API);
-});
-
-
 
 
 // server.listen(process.env.PORT_API, () => {
@@ -132,8 +140,3 @@ server.listen(process.env.PORT_API, () => {
 // app.listen(process.env.PORT_API, () => {
 //     console.log("API http://localhost:" + process.env.PORT_API);
 // })
-
-console.log(os.hostname());
-console.log(os.networkInterfaces());
-console.log(os.userInfo());
-console.log(os.version());
